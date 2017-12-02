16a18052498e6f35a4b46a1374ae9c1eba2487e3
chiaremar <chiarella.marianne@gmail.com>
2017-12-02 13:35:43 -0500

    express generated server template

---
+++ mbta_server/dbconnection.js
@@ -0,0 +1,31 @@
+var express    = require("express");
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'mydb'
+});
+var app = express();
+
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");    
+} else {
+    console.log("Error connecting database ... nn");    
+}
+});
+
+app.get("/",function(req,res){
+connection.query('SELECT DISTINCT route from RoutesLinesMap', function(err, rows, fields) {
+//connection.end();
+  if (!err)
+//    console.log('The solution is: ', rows);
+    console.log('Success while performing Query.');
+	res.send(rows);
+  else
+    console.log('Error while performing Query.');
+  });
+});
+
+app.listen(3000);
