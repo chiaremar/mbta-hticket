bb681fd0c559b6addb2c2bca49d3410331214c79
chiaremar <chiarella.marianne@gmail.com>
2017-12-03 18:52:21 -0500

    fixed body-parser issue where json was undefined in post

--- mbta_client/src/pages/login/login.ts
+++ mbta_client/src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController,AlertController, NavParams } from 'ionic-angular';
+import {AuthServiceProvider} from '../../providers/auth-service/auth-service';
+import {HomePage} from '../../pages/home/home';
 /**
  * Generated class for the LoginPage page.
  *
@@ -15,8 +16,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  registerCredentials = { email: '', password: '' };
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, public navParams: NavParams, private auth: AuthServiceProvider) {
   }
+
+  public createAccount() {
+    this.navCtrl.push('RegisterPage');
+  }
+ 
+  public login() {
+    
+    this.auth.login(this.registerCredentials).subscribe(allowed => {
+      if (allowed) {  
+             
+        this.navCtrl.setRoot(HomePage);
+      } else {
+        this.showError("Access Denied");
+      }
+    },
+      error => {
+        this.showError(error);
+      });
+  }
+ 
+  showError(text) {
+ 
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
--- mbta_server/dbconnection.js
+++ mbta_server/dbconnection.js
@@ -1,14 +1,28 @@
-var express    = require("express");
+var express    = require('express');
+
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+
+var bodyParser = require('body-parser');
+var app = express();
+//app.use(bodyParser.urlencoded({extended:false}));
+//app.use(bodyParser.json());
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+var db = mysql.createConnection({
   host      : 'localhost',
   user      : 'root',
   password  : 'password',
   database  : 'mydb'
 });
-var app = express();
 
-connection.connect(function(err){
+
+
+db.connect(function(err){
 if(!err) {
     console.log("Database is connected ... nn");    
 } else {
@@ -16,15 +30,71 @@ if(!err) {
 }
 });
 
-app.get("/",function(req,res){
-connection.query('SELECT DISTINCT route from RoutesLinesMap', function(err, rows, fields) {
 
-  if (!err)
-    console.log('Success while performing Query.');
-	res.send(rows);
-  else
-    console.log('Error while performing Query.');
-  });
-});
 
 app.listen(3000);
+
+//register user
+app.get('/register', (req, res) => {
+  let post = {username:'marianne', password:'12345'};
+  let sql = 'INSERT INTO users SET ?';
+  let query = db.query(sql, post, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send('User registered...');
+  })
+});
+
+
+//login user
+app.post('/login', jsonParser, function(req, res)  {
+//  let post = {username:'marianne', password:'12345'};
+  var usern=req.body.username;
+  var passw=req.body.password;
+  console.log(req.body.username);
+  console.log(req.query);
+
+  let sql = `SELECT * from users where username='${usern}' and password='${passw}';`;
+  console.log(sql);
+
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send('User logged on...');
+  })
+});
+
+// app.get('/login',function(req,res){
+//   let sql = 'SELECT * from users where username = req.username'
+//   db.query('SELECT DISTINCT route from RoutesLinesMap', function(err, rows, fields) {
+  
+//     if (!err) {
+//       console.log('Success while performing Query.');
+//       res.send(rows);
+//     } else
+//       console.log('Error while performing Query.');
+//     });
+//   });
+
+//get all stops (unique)
+app.get('/routes',(req,res) => {
+  let sql = 'SELECT DISTINCT route from RoutesLinesMap';
+  let query = db.query(sql, (err, results) => {
+    if (err) throw err;
+      console.log('routes fetched');
+      res.send(results);
+    });
+});
+
+//get filtered list of stops for start route
+app.get('/routes/:routename',(req,res) => {
+  let sql = `SELECT DISTINCT route from RoutesLinesMap 
+             WHERE RoutesLinesMap.line 
+             IN (select line from RoutesLinesMap where route = ${req.params.routename})`;
+  let query = db.query(sql, (err, results) => {
+    if (err) throw err;
+      console.log('filtered routes fetched');
+      res.send(results);
+    });
+});
+
